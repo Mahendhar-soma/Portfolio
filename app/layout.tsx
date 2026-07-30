71b2cf007b18bfcac45d1aedb5911c679fda8042
letusmob <letusmob@gmail.com>
2026-07-30 12:23:53 +0530

    30 july commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const syne = Syne({
   subsets: ["latin"],
   variable: "--font-syne",
   display: "swap",
-  weight: ["400", "500", "600", "700", "800"], 
+  weight: ["400", "500", "600", "700", "800"],
 });
 
 const outfit = Outfit({
